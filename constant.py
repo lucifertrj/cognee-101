c33ec0c504664f1be96c36a7262a18a7f4204e25
lucifertrj <jaintarun.abd17@gmail.com>
2026-01-06 00:34:09 +0530

    raw reference as context

---
+++ constant.py
@@ -0,0 +1,6 @@
+MY_PREFERENCE = """
+- I like to visit places near the beach where I can find the best spots. 
+- I need locations that are rare to find on blogs but are goldmine places for your eyes
+- I prefer Vegetarian meals. Use this when I ask for restaurants recommendation
+- My hobbies that might also help in planning Itineraries: I love Anime, F1 and Cricket.
+"""
