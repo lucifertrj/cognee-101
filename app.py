a4542dafccac207be9b622e7a3fe67a8f3172cbc
lucifertrj <jaintarun.abd17@gmail.com>
2026-01-06 00:35:12 +0530

    default creds: app.py, qdrant and gemini custom.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import asyncio
+from constant import MY_PREFERENCE
+
+import cognee
+from cognee.api.v1.visualize.visualize import visualize_graph
+
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    await cognee.prune.prune_data()
+    await cognee.prune.prune_system(metadata=True)
+
+    await cognee.add(MY_PREFERENCE)
+    # Used for grouping related data points in the knowledge graph.
+    # await cognee.add(MY_PREFERENCE,node_set="personal_tarun")
+    # await cognee.add("- In Food I usually prefer Thali and Indian Vegetarian food places",node_set=["food"])
+
+    await cognee.cognify()
+    await visualize_graph("./graph_after_cognify.html")
+    await cognee.memify() # enhance the knowledge graph with memory consolidation for improved connections
+    
+    results = await cognee.search("plan 3 days Itinerary for Rome")
+    for result in results:
+        print(result) 
+
+if __name__ == '__main__':
+    asyncio.run(main())
